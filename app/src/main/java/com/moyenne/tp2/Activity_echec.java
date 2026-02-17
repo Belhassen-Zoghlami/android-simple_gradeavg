64081ca9b283cb338e39e860dd3494425af0d58f
Belhassen-Zoghlami <6belhassenzoghlami9@gmail.com>
2026-02-17 01:22:27 +0100

    validation-less functional sms/act switch

--- app/src/main/java/com/moyenne/tp2/Activity_echec.java
+++ app/src/main/java/com/moyenne/tp2/Activity_echec.java
@@ -9,19 +9,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.telephony.SmsManager;
 
 public class Activity_echec extends AppCompatActivity {
-
+    public String m;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_echec);
         TextView Ft = findViewById(R.id.FText);
-        String m = getIntent().getStringExtra("msg");
+        m = getIntent().getStringExtra("msg");
         Ft.setText(m);
     }
     public void sendsms(View v)
     {
-        //+1 555-123-4567
-        send
+        SmsManager sm = SmsManager.getDefault();
+        sm.sendTextMessage("5551234567",null,m,null,null);
     }
 }
--- app/src/main/java/com/moyenne/tp2/Activity_reussi.java
+++ app/src/main/java/com/moyenne/tp2/Activity_reussi.java
@@ -1,6 +1,8 @@
 package com.moyenne.tp2;
 
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -10,14 +12,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Activity_reussi extends AppCompatActivity {
-
+    public String m;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_reussi);
         TextView sucT = findViewById(R.id.SuccT);
-        String m = getIntent().getStringExtra("msg");
+        m = getIntent().getStringExtra("msg");
         sucT.setText(m);
+    }
+
+    public void sendsms(View v)
+    {
+        SmsManager sm = SmsManager.getDefault();
+        sm.sendTextMessage("5551234567",null,m,null,null);
     }
 }
